f624aeb15f49099dbda2a4ac727c47d2993febdb
Hubert Nguyen <hubertanguyen@gmail.com>
2015-04-29 00:17:17 -0500

    Refactored animation.js, added comments

--- public/animation.js
+++ public/animation.js
@@ -1,13 +1,17 @@
 // Animations
 $(document).ready(function() {
 
+  // Handler for button click
   $("button").on('click',function(){
+    // Bounce the hippo logo
     $(".navLogo").effect( "bounce", {times:6}, 3500 );
+    // Fades in new speech bubble text
     var hippoFood = $('.food-type').val();
     $(".bubble-text").fadeOut(1500,function(){
       $(".bubble-text").html("Yummy! Some " + hippoFood + "!");
     }).fadeIn(1500);
 
+    // Fades out placeholder input text, and adjust padding
     $('b').fadeOut('slow');
     $('#input-1').css('padding','1px');
   });
